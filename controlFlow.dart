7a497534daccaf7953a5de807754ce6b8ceaf0d1
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-05-03 13:40:57 +0100

    .

--- controlFlow.dart
+++ controlFlow.dart
@@ -1,12 +1,15 @@
+bool isAllowed = false;
 void main() {
   // If Statements
+  isAllowed = !isAllowed;
 
-  int age = 30;
+  int age = 20;
 
-  if(age >= 18) {
-    print('Adult');
+  // !isAllowed -> false
+  if(age != 18 && isAllowed) {
+    print('oops!');
   } else {
-    print('Child');
+    print('haaa!');
   }
  
 }
