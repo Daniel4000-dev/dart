1d055e02226a22cafcc01b1f62b708a774fa8113
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2024-05-03 14:05:02 +0100

    .

---
+++ exercise1.dart
@@ -0,0 +1,26 @@
+void main() {
+/*
+Exercise:
+Develop a program to calculate the shipping cost based on
+the destination zone and the weight of the packge (you will be provided)
+calculate the shipping cost according to these conditions:
+  If the destination zone is 'XYZ', the shipping cost is $5 per kilogram.
+  If the destination zone is 'ABC', the shipping cost os $7 per kilogram.
+  If the destination zone is 'PQR', the shipping cost is $10 per kilogram.
+  If the destination zone is not 'XYZ', 'ABC', or 'PQR', display an error message
+  */
+
+  const destinationZone = 'PQR';
+  const weightInKgs = 6;
+
+  if(destinationZone == 'PQR') {
+    print('Shipping Cost: ${weightInKgs * 10}');
+  } else if(destinationZone == 'ABC') {
+    print('Shipping Cost: ${weightInKgs * 7}');
+  } else if(destinationZone == 'XYZ') {
+    print('Shipping Cost: ${weightInKgs * 5}');
+  } else {
+    print('No Specified Location');
+  }
+ 
+}
